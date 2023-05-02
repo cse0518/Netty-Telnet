45ecc7162e34f7652e64e92d21a11cef84011a6e
cse0518 <g787@naver.com>
2023-05-02 17:57:25 +0900

    feat: netty 소켓 연결 로직 수정

---
+++ netty-client/src/main/java/com/humuson/tcpclient/config/NettyClientConfig.java
@@ -0,0 +1,41 @@
+package com.humuson.tcpclient.config;
+
+import com.humuson.tcpclient.handler.TcpClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class NettyClientConfig {
+
+    @Bean
+    public EventLoopGroup group() {
+        return new NioEventLoopGroup();
+    }
+
+    @Bean
+    public Bootstrap bootstrap(EventLoopGroup group) {
+        Bootstrap b = new Bootstrap();
+        return b.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(
+                                new StringEncoder(StandardCharsets.UTF_8),
+                                new TcpClientHandler()
+                        );
+                    }
+                });
+    }
+}
--- netty-client/src/main/java/com/humuson/tcpclient/consumer/Consumer.java
+++ netty-client/src/main/java/com/humuson/tcpclient/consumer/Consumer.java
@@ -1,23 +1,66 @@
 package com.humuson.tcpclient.consumer;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Slf4j
 @Component
 public class Consumer {
 
-    private final ChannelFuture netty;
+    private final String HOST;
+    private final int PORT;
+    private final Bootstrap bootstrap;
+    private final EventLoopGroup group;
+    private Channel channel;
 
-    public Consumer(ChannelFuture netty) {
-        this.netty = netty;
+    public Consumer(@Value(value = "${netty.host}") String HOST,
+                    @Value(value = "${netty.port}") int PORT,
+                    Bootstrap bootstrap,
+                    EventLoopGroup group) {
+        this.HOST = HOST;
+        this.PORT = PORT;
+        this.bootstrap = bootstrap;
+        this.group = group;
+    }
+
+    @PostConstruct
+    private void connect() throws InterruptedException {
+        ChannelFuture channelFuture = bootstrap.connect(HOST, PORT).sync();
+        if (channelFuture.isSuccess()) {
+            System.out.println("## Successfully connected to Netty server");
+            channel = channelFuture.channel();
+        } else {
+            System.out.println("## Failed to connect to Netty server");
+            throw new InterruptedException();
+        }
     }
 
     @KafkaListener(topics = "${spring.kafka.topic.name}", containerFactory = "myKafkaListenerContainerFactory")
-    public void consume(String message) {
+    public void consume(String message) throws InterruptedException {
+        if (channel == null) {
+            connect();
+        }
+
         log.info("### consume data : {}", message);
-        netty.channel().writeAndFlush(message);
+        ByteBuf byteBuf = Unpooled.copiedBuffer(message, CharsetUtil.UTF_8);
+        channel.writeAndFlush(byteBuf);
+    }
+
+    @PreDestroy
+    public void shutdown() throws InterruptedException {
+        channel.close().sync();
+        group.shutdownGracefully();
     }
 }
